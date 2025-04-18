39b1570061b314f99e2d71e237a55459534ed393
TCArianGabriel <daniel.tovar@unmsm.edu.pe>
2025-04-17 21:28:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fcomputers/connection/FirebaseConnection.java
@@ -0,0 +1,29 @@
+package com.example.fcomputers.connection;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class FirebaseConnection {
+
+    private static FirebaseAuth mAuth;
+
+    private static StorageReference mStorageRef;
+
+    private static FirebaseFirestore db;
+
+    public static FirebaseAuth ConnectionAuth(){
+        return mAuth = FirebaseAuth.getInstance();
+    }
+
+    public static StorageReference ConnectionStorage(){
+        return mStorageRef = FirebaseStorage.getInstance().getReference();
+    }
+
+    public static FirebaseFirestore ConnectionFirestore(){
+        return db = FirebaseFirestore.getInstance();
+    }
+
+}
